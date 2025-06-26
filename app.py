691c2820b78d97259e3f6930eebdd2d4a859e0a0
Mihai Rosu <roshu.mihai@yahoo.com>
2025-06-26 16:01:08 +0300

    Corectat port app.py

--- app.py
+++ app.py
@@ -343,4 +343,6 @@ def api_toggle_like(id):
 # Pornim aplicația
 if __name__ == "__main__":
     init_db()
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
